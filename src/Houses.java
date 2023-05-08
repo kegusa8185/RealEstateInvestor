96a118640f3174656d92977179cc878de6294e44
kegusa8185 <kegusa8185@student.pps.net>
2023-05-08 09:53:52 -0700

    Initial commit

---
+++ src/Houses.java
@@ -0,0 +1,16 @@
+public class Houses {
+    public static void main(String[] args) {
+        new Houses();
+    }
+    private int bed;
+    private double bath;
+    private int sqft;
+    private int price;
+    public Houses(int bed, double bath, int sqft, int price){
+        this.bed = bed;
+        this.bath = bath;
+        this.sqft = sqft;
+        this.price = price;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args){
+        new Main();
+    }
+    public Main(){
+        //Game();
+    }
+    public void game(){
+        int years = 0;
+        int playerNetWorth = 0;
+        int income = 0;
+        int loanAmount = 0;
+        int debt = 0;
+
+    }
+}
