e730a7edc199227d1f48758661c5bfab764b16e8
kegusa8185 <kegusa8185@student.pps.net>
2023-05-10 09:57:41 -0700

    worked on some methods n stuff

--- src/Houses.java
+++ src/Houses.java
@@ -2,15 +2,50 @@ public class Houses {
     public static void main(String[] args) {
         new Houses();
     }
+
     private int bed;
     private double bath;
     private int sqft;
-    private int price;
-    public Houses(int bed, double bath, int sqft, int price){
+    private double value;
+    private int location;
+
+    public Houses(int bed, double bath, int sqft, double value, int location) {
         this.bed = bed;
         this.bath = bath;
         this.sqft = sqft;
-        this.price = price;
+        this.value = value;
+        this.location = location;
+    }
+    public double getBath() {
+        return bath;
+    }
+    public int getBed(){
+        return bed;
+    }
+    public int getSqft(){
+        return sqft;
+    }
+    public double getValue(){
+        return value;
+    }
+    public int getLocation(){
+        return location;
+    }
+    public void setValue(double newVal){
+        value = newVal;
+    }
+    public int plusOrMinus(){
+        int rand = (int) (Math.random() * 2);
+        if (rand == 0){
+            return -1;
+        }
+        else{
+            return 1;
+        }
 
     }
+    public double getTermPercentIncrease(){
+        return (Math.random() + (plusOrMinus() * (getLocation() / 5.0 )));
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
+import java.util.ArrayList;
 public class Main {
+    double years = 0;
+    int playerNetWorth = 0;
+    int income = 0;
+    int loanAmount = 0;
+    int debt = 0;
+    ArrayList<Houses> property = new ArrayList<Houses>();
     public static void main(String[] args){
         new Main();
     }
     public Main(){
         //Game();
     }
+
     public void game(){
-        int years = 0;
-        int playerNetWorth = 0;
-        int income = 0;
-        int loanAmount = 0;
-        int debt = 0;
 
     }
+    public void nextTerm(){
+        for (int i = 0; i < property.size(); i++) {
+
+        }
+    }
+
 }
